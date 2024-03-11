7e1f697fefd59618f519ff13b11ec08686a13633
Don Alvin <alvindon41@gmail.com>
2024-03-11 14:13:58 +0300

    create user

--- client/src/auth/AuthProviderWithNavigate.tsx
+++ client/src/auth/AuthProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import { ReactNode } from "react";
 
@@ -6,6 +7,7 @@ type Props = {
 }
 
 const AuthProviderWithNavigate = ({children} : Props) => {
+    const { createUser, isError, isLoading, isSuccess } = useCreateMyUser()
     const domain = import.meta.env.VITE_AUTH0_DOMAIN
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
@@ -15,7 +17,9 @@ const AuthProviderWithNavigate = ({children} : Props) => {
     }
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("USER", user);
+        if(user?.sub && user?.email) {
+            createUser({auth0Id: user.sub, email: user.email})
+        }
         
     }
 
